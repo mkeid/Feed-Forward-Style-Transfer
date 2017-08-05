19bb2bf5e33e352cc6fc3463dbee7d0cccb1477e
Mohamed K. Eid <mohamedkeid@gmail.com>
2017-08-05 16:48:41 -0700

    added doc for generator

--- src/generator.py
+++ src/generator.py
@@ -1,46 +1,67 @@
-"""
-    Author: Mohamed K. Eid (mohamedkeid@gmail.com)
-    Description: generative model with the architectural specifications suited for artistic style transfer
-"""
 import tensorflow as tf
 
-# Model hyperparameters
+# Model hyper-parameters
 DECAY = .9
 EPSILON = 1e-8
 
 
 class Generator:
+    """Generative model with the architectural specifications suited for artistic style transfer."""
+
     def __init__(self, is_training=True):
         self.training = is_training
 
-    # Constructs the generative network's layers. Normally called after initialization.
     def build(self, img):
-        self.padded = self.__pad(img, 40)
+        """Constructs the generative network's layers. Normally called after initialization.
+        
+        Args:
+            img: 4D tensor representation of image batch
+        """
 
-        self.conv1 = self.__conv_block(self.padded, maps_shape=[9, 9, 3, 32], stride=1, name='conv1')
-        self.conv2 = self.__conv_block(self.conv1, maps_shape=[2, 2, 32, 64], stride=2, name='conv2')
-        self.conv3 = self.__conv_block(self.conv2, maps_shape=[2, 2, 64, 128], stride=2, name='conv3')
+        self.padded = self._pad(img, 40)
 
-        self.resid1 = self.__residual_block(self.conv3, maps_shape=[3, 3, 128, 128], stride=1, name='resid1')
-        self.resid2 = self.__residual_block(self.resid1, maps_shape=[3, 3, 128, 128], stride=1, name='resid2')
-        self.resid3 = self.__residual_block(self.resid2, maps_shape=[3, 3, 128, 128], stride=1, name='resid3')
-        self.resid4 = self.__residual_block(self.resid3, maps_shape=[3, 3, 128, 128], stride=1, name='resid4')
-        self.resid5 = self.__residual_block(self.resid4, maps_shape=[3, 3, 128, 128], stride=1, name='resid5')
+        self.conv1 = self._conv_block(self.padded, maps_shape=[9, 9, 3, 32], stride=1, name='conv1')
+        self.conv2 = self._conv_block(self.conv1, maps_shape=[2, 2, 32, 64], stride=2, name='conv2')
+        self.conv3 = self._conv_block(self.conv2, maps_shape=[2, 2, 64, 128], stride=2, name='conv3')
 
-        self.conv4 = self.__upsample_block(self.resid5, maps_shape=[2, 2, 64, 128], stride=2, name='conv4')
-        self.conv5 = self.__upsample_block(self.conv4, maps_shape=[2, 2, 32, 64], stride=2, name='conv5')
-        self.conv6 = self.__conv_block(self.conv5, maps_shape=[9, 9, 32, 3], stride=1, name='conv6', activation=None)
+        self.resid1 = self._residual_block(self.conv3, maps_shape=[3, 3, 128, 128], stride=1, name='resid1')
+        self.resid2 = self._residual_block(self.resid1, maps_shape=[3, 3, 128, 128], stride=1, name='resid2')
+        self.resid3 = self._residual_block(self.resid2, maps_shape=[3, 3, 128, 128], stride=1, name='resid3')
+        self.resid4 = self._residual_block(self.resid3, maps_shape=[3, 3, 128, 128], stride=1, name='resid4')
+        self.resid5 = self._residual_block(self.resid4, maps_shape=[3, 3, 128, 128], stride=1, name='resid5')
+
+        self.conv4 = self._upsample_block(self.resid5, maps_shape=[2, 2, 64, 128], stride=2, name='conv4')
+        self.conv5 = self._upsample_block(self.conv4, maps_shape=[2, 2, 32, 64], stride=2, name='conv5')
+        self.conv6 = self._conv_block(self.conv5, maps_shape=[9, 9, 32, 3], stride=1, name='conv6', activation=None)
 
         self.output = tf.nn.sigmoid(self.conv6)
 
-    # Returns a variable for weights wiht a specified filters shape
     @staticmethod
-    def __get_weights(shape):
-        return tf.Variable(tf.truncated_normal(shape, mean=0., stddev=.1), dtype=tf.float32)
+    def _get_weights(shape):
+        """Returns a variable for weights with a specified filters shape.
+        
+        Args:
+            shape: a list specifying the initialized weights shape
+            
+        Returns:
+            weights: tf.Variable representing a set of weights with a normal distribution
+        """
 
-    # Instance normalize inputs to reduce covariate shift and reduce dependency on input contrast to improve results
+        init = tf.truncated_normal(shape, mean=0., stddev=.1)
+        weights = tf.Variable(init, dtype=tf.float32)
+        return weights
+
     @staticmethod
-    def __instance_normalize(inputs):
+    def _instance_normalize(inputs):
+        """Instance normalize inputs to reduce covariate shift and reduce dependency on input contrast to improve results.
+        
+        Args:
+            inputs: 4D tensor representing image layer encodings
+            
+        Returns:
+            maps: 4D tensor of batch normalized inputs
+        """
+
         with tf.variable_scope('instance_normalization'):
             batch, height, width, channels = [_.value for _ in inputs.get_shape()]
             mu, sigma_sq = tf.nn.moments(inputs, [1, 2], keep_dims=True)
@@ -48,17 +69,38 @@ class Generator:
             shift = tf.Variable(tf.constant(.1, shape=[channels]))
             scale = tf.Variable(tf.ones([channels]))
             normalized = (inputs - mu) / (sigma_sq + EPSILON) ** .5
+            maps = scale * normalized + shift
+            return maps
 
-            return scale * normalized + shift
-
-    # Pads input of the image so the output is the same dimensions even after upsampleolution
     @staticmethod
-    def __pad(inputs, size):
-        return tf.pad(inputs, [[0, 0], [size, size], [size, size], [0, 0]], "REFLECT")
+    def _pad(inputs, size):
+        """Pads input of the image so the output is the same dimensions even after strided convolution.
+        
+        Args:
+            inputs: 4D tensor representing image layer encodings
+            size: int specifying the pad size
+            
+        Returns:
+            padded_inputs: 4D tensor of padded inputs
+        """
 
-    # Batch normalize inputs to reduce covariate shift and improve the efficiency of training
+        padded_inputs = tf.pad(inputs, [[0, 0], [size, size], [size, size], [0, 0]], "REFLECT")
+        return padded_inputs
+
     @staticmethod
-    def __batch_normalize(inputs, num_maps, is_training):
+    def _batch_normalize(inputs, num_maps, is_training):
+        """Batch normalize inputs to reduce covariate shift and improve the efficiency of training.
+        
+        Args:
+            inputs: 4D tensor representing image layer encodings
+            num_maps: int representing the number of input feature maps
+            is_training: bool representing whether or not the model is 
+                         being trained rather than being used for inference
+            
+        Returns:
+            bn_inputs: 4D tensor of batch normalized inputs
+        """
+
         with tf.variable_scope("batch_normalization"):
             # Trainable variables for scaling and offsetting our inputs
             scale = tf.Variable(tf.ones([num_maps], dtype=tf.float32))
@@ -87,30 +129,54 @@ class Generator:
 
         return bn_inputs
 
-    # Convolve inputs and return their batch normalized tensor
-    def __conv_block(self, inputs, maps_shape, stride, name, norm=True, padding='SAME', activation=tf.nn.relu):
+    def _conv_block(self, inputs, maps_shape, stride, name,
+                    norm=True, padding='SAME', activation=tf.nn.relu):
+        """Convolve inputs and return their batch normalized tensor.
+        
+        Args:
+            inputs: 4D tensor representing image layer encodings
+            maps_shape: list representing the shape of the layer weights
+            stride: int representing stride length
+            name: string assigned as the tf op names
+            norm: bool representing whether or not to normalize layer inputs
+            padding: string representing padding type
+            activation: tf.nn activation function
+            
+        Returns:
+            maps: 4D tensor representing convolved feature maps
+        """
+
         with tf.variable_scope(name):
             if name == 'output':
                 activation = tf.nn.sigmoid
 
-            filters = self.__get_weights(maps_shape)
+            filters = self._get_weights(maps_shape)
             filter_maps = tf.nn.conv2d(inputs, filters, [1, stride, stride, 1], padding=padding)
             num_out_maps = maps_shape[3]
             bias = tf.Variable(tf.constant(.1, shape=[num_out_maps]))
             filter_maps = tf.nn.bias_add(filter_maps, bias)
 
             if norm:
-                filter_maps = self.__instance_normalize(filter_maps)
+                filter_maps = self._instance_normalize(filter_maps)
 
-            if activation:
-                return activation(filter_maps)
-            else:
-                return filter_maps
+            maps = activation(filter_maps) if activation else filter_maps
+            return maps
 
-    # Upsamples inputs using transposed convolution
-    def __upsample_block(self, inputs, maps_shape, stride, name):
+    def _upsample_block(self, inputs, maps_shape, stride, name):
+        """Upsamples inputs using transposed convolution.
+        
+        Args:
+            inputs: 4D tensor representing image layer encodings
+            maps_shape: list representing the shape of the layer weights
+            stride: int representing stride length
+            name: string assigned as the tf op names
+            
+        Returns:
+            maps: 4D tensor representing upsampled feature maps
+        """
+
         with tf.variable_scope(name):
-            filters = self.__get_weights(maps_shape)
+            filters = self._get_weights(maps_shape)
 
             # Get dimensions to use for the upsample operator
             batch, height, width, channels = inputs.get_shape().as_list()
@@ -124,18 +190,32 @@ class Generator:
             upsample = tf.nn.conv2d_transpose(inputs, filters, output_shape=out_shape, strides=stride)
             bias = tf.Variable(tf.constant(.1, shape=[out_size]))
             upsample = tf.nn.bias_add(upsample, bias)
-            bn_maps = self.__instance_normalize(upsample)
+            bn_maps = self._instance_normalize(upsample)
+            maps =  tf.nn.relu(bn_maps)
 
-            return tf.nn.relu(bn_maps)
+            return maps
 
-    # The residual blocks is comprised of two convolutional layers and aims to add long short-term memory to the network
-    def __residual_block(self, inputs, maps_shape, stride, name):
+    def _residual_block(self, inputs, maps_shape, stride, name):
+        """Residual block comprised of two conv layers and aims to add long short-term memory to the network.
+        
+        Args:
+            inputs: 4D tensor representing image layer encodings
+            maps_shape: list representing the shape of the layer weights
+            stride: int representing stride length
+            name: string assigned as the tf op names
+            
+        Returns:
+            maps: 4D tensor representing feature maps
+        """
+
         with tf.variable_scope(name):
-            conv1 = self.__conv_block(inputs, maps_shape, stride=stride, padding='VALID', name='c1')
-            conv2 = self.__conv_block(conv1, maps_shape, stride=stride, padding='VALID', name='c2', activation=None)
+            conv1 = self._conv_block(inputs, maps_shape, stride=stride, padding='VALID', name='c1')
+            conv2 = self._conv_block(conv1, maps_shape, stride=stride, padding='VALID', name='c2', activation=None)
 
             batch = inputs.get_shape().as_list()[0]
             patch_height, patch_width, num_filters = conv2.get_shape().as_list()[1:]
             out_shape = tf.stack([batch, patch_height, patch_width, num_filters])
             cropped_inputs = tf.slice(inputs, [0, 1, 1, 0], out_shape)
-            return conv2 + cropped_inputs
+            maps = conv2 + cropped_inputs
+
+            return maps
